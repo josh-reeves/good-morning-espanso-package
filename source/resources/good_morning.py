747ddee331438ed0848c7cdcea02c744d3dcd7be
Joshua Reeves <145219625+josh-reeves@users.noreply.github.com>
2025-06-22 22:23:51 -0500

    Cleaned up some comments.

--- source/resources/good_morning.py
+++ source/resources/good_morning.py
@@ -20,10 +20,10 @@ weekdayGreetings = config["weekdayGreetings"]
 mondayGreetings = config["mondayGreetings"]
 fridayGreetings = config["fridayGreetings"]
 
-# For each name in the names file, check to see if the name includes tagChar. If the name is tagged, add it to the tagged list in the 
-# order it appears in the original file. Otherwise, either append or prepend the name to the shuffle list depending on whether a 
-# random value between 0 and 99999 is even or odd. This builds the list in a semi-random order and prevents the need to shuffle the 
-# values later on:
+# For each name in the names file, check to see if tagged is set to true. If the name is tagged, add it to the tagged list
+# along with its birthday in the order it appears in the original file. Otherwise, either append or prepend the name and birthday
+# to the shuffle list depending on whether a random value between 0 and 99999 is even or odd. This builds the list in a 
+# semi-random order and prevents the need to shuffle the values later on:
 namesFile = open(file=os.path.join(scriptDir, "names.csv"), newline="", encoding="utf-8-sig")
 
 tagged: LinkedList = LinkedList()
@@ -43,7 +43,8 @@ for row in reader:
 
 namesFile.close()
 
-# Output list of names, prepending each with a random value from the personalGreetings array:
+# Output each name in the tagged and shuffle lists. Check to see if the birth day and month match today. If they do,
+# prepend the name with a greeting from birthdayGreetings. Otherwise, prepend each name with a random value from personalGreetings:
 for name in (tagged + shuffle):
     if (name["birthday"] == datetime.datetime.now().strftime("%m/%d")):
         print(f"{random.choice(birthdayGreetings)} {name['name']}!")
